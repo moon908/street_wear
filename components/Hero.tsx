443b0fd54fd581f4f65907ebcdf72c8b2a17f4c6
Siddhesh Moon <siddheshmoon908u@gmail.com>
2026-01-19 17:33:34 +0530

    Implementing the scale and animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
     return (
         <div className='relative w-full h-screen overflow-hidden'>
             <div className='absolute bottom-0 left-[32%]'>
-                <img src="/firstHero.png" alt="Hero Image" width={!isMobile ? 530 : 650} className="drop-shadow-[0_45px_70px_rgba(0,0,0,0.7)]" />
+                <img src="/firstHero.png" alt="Hero Image" width={!isMobile ? 530 : 720} className="drop-shadow-[0_45px_70px_rgba(0,0,0,0.7)]" />
             </div>
             <div className={isMobile ? 'absolute top-[22%] left-[5%] flex gap-6 font-averia-gruesa-libre text-7xl' : 'absolute top-[25%] left-[22%] flex gap-90 font-averia-gruesa-libre text-9xl'}>
                 <h1>
@@ -30,7 +30,7 @@ const Hero = () => {
                     Discover the latest trends in streetwear. Premium quality, unique designs, and the perfect fit for your style. High Quality fabrics and unique designs. All day comfort and trendy look and Easy to style with any outfit
                 </p>)}
                 <div>
-                    <button className='font-quicksand border-2 rounded-2xl text-lg font-medium mt-5 w-36 h-9 flex items-center justify-center gap-2'>
+                    <button className='font-quicksand border-2 rounded-2xl text-lg font-medium mt-5 w-36 h-9 flex items-center justify-center gap-2 cursor-pointer transition-transform hover:scale-105'>
                         Shop Now <FaShoppingBag className='ml-2' />
                     </button>
                 </div>
@@ -41,7 +41,7 @@ const Hero = () => {
                         <img src="/herosub.png" alt="Jacket" width={220} className='ml-6 mt-1' />
                     </div>
                     <div className='absolute border-2 rounded-2xl bottom-[5%] right-[11.5%] bg-transparent w-80 h-10'>
-                        <p className='font-quicksand text-lg font-medium flex justify-center items-center w-full h-full'>
+                        <p className='font-quicksand text-lg font-medium flex justify-center items-center w-full h-full cursor-pointer transition-transform hover:scale-105'>
                             New Collection <FaLocationArrow className='ml-3' />
                         </p>
                     </div>
